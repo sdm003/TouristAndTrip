b90f91f85cc5b23e7cd687b6177a6281e457eeb2
sdm003 <daniyar743@gmail.com>
2019-06-06 20:08:22 +0600

    update database to Herouku

--- src/main/java/com/example/TouristTrip/repository/AgreementRepository.java
+++ src/main/java/com/example/TouristTrip/repository/AgreementRepository.java
@@ -2,7 +2,6 @@ package com.example.TouristTrip.repository;
 
 import com.example.TouristTrip.entity.Agreement;
 import org.springframework.data.jpa.repository.JpaRepository;
-import sun.management.jdp.JdpPacket;
 
 public interface AgreementRepository extends JpaRepository<Agreement,Long> {
 }
